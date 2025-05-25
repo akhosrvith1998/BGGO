0413b4c1d51ac7876b29beaa8787979cb08c1d78
akhosrvith1998 <a.khosrvith1898@gmail.com>
2025-05-25 14:10:03 +0330

    Create Constants.py

---
+++ Constants.py
@@ -0,0 +1,6 @@
+# Constants for the bot
+BOT_TOKEN = "7889701836:AAECLBRjjDadhpgJreOctpo5Jc72ekDKNjc"
+BOT_USERNAME = "Bgnabot"
+BOT_NAME = "GB"
+OWNER_ID = "7824772776"
+BASE_URL = f"https://api.telegram.org/bot{BOT_TOKEN}/"
